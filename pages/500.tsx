232add93320d1c31d44956b7b91969c67a3d4207
Petko4 <petr.vaclavik4@gmail.com>
2023-05-10 21:38:34 +0200

    Custom error pages

---
+++ pages/500.tsx
@@ -0,0 +1,14 @@
+// visible only in Prodution
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Error 😥</h1>
+      <p>
+        Looks like something went wrong. Please refresh thee page or contact
+        support!
+      </p>
+    </div>
+  );
+};
+
+export default ErrorPage;
